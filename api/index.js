1b2b4269f9f90b0eaed3089fcbe355e018134ea6
kishore <kishoreram10106@gmail.com>
2025-02-03 20:00:55 +0530

    add update user api route

--- api/index.js
+++ api/index.js
@@ -3,6 +3,7 @@ import mongoose from "mongoose";
 import dotenv from "dotenv";
 import userRouters from "./routes/user.route.js"
 import authRouters from "./routes/auth.route.js"
+import cookieParser from 'cookie-parser';
 
 dotenv.config()
 
@@ -17,6 +18,7 @@ mongoose
 
 const app = express();
 app.use(express.json());
+app.use(cookieParser());
 
 app.use("/api/user",userRouters)
 app.use("/api/auth",authRouters)
