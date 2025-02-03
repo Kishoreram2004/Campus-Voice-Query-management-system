7f64ace988ef4e3bc21d76ee64b6d7862606c891
kishore <kishoreram10106@gmail.com>
2025-02-03 20:34:23 +0530

    add admin fucntionality to the user

--- api/models/user.model.js
+++ api/models/user.model.js
@@ -17,6 +17,18 @@ const userSchema = new mongoose.Schema({
     profilePicture: {
         type: String,
         default: 'https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png',
+    },
+    isAdmin: {
+        type: Boolean,
+        default: false,
+    },
+    isStudent:{
+        type:Boolean,
+        default:false,
+    },
+    isStaff:{
+        type:Boolean,
+        default:true,
     }
     }, {timestamps: true}
 );
