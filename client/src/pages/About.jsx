aa17fdd43edaf60a87d39fb310c657cd5e28761c
kishore <kishoreram10106@gmail.com>
2025-02-06 20:42:21 +0530

    complete about

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -2,8 +2,29 @@ import React from 'react'
 
 const About = () => {
   return (
-    <div>About</div>
-  )
+<div className='min-h-screen flex items-center justify-center my-0 '>
+      <div className='max-w-2xl mx-auto p-3 text-center'>
+        <div>
+          <h1 className='text-3xl font font-semibold text-center my-7'>
+          About Campus Voice Query Management System 
+          </h1>
+          <div className='text-xl text-gray-500 flex flex-col gap-6'>
+            <p>
+            The Campus Voice Query Management System is an innovative platform designed to simplify communication within the campus. It allows students, faculty, and staff to raise queries using voice input and receive timely responses from the concerned authorities.
+            </p>
+            <p>
+            With features like real-time query tracking, automated categorization, and instant notifications, our system ensures a seamless and efficient resolution process. Whether it’s academic assistance, administrative support, or general inquiries, we make campus interactions more accessible and hassle-free.
+            </p>
+            <p>
+            Our goal is to enhance campus engagement by bridging the gap between students and management through a smart, responsive, and user-friendly interface.
+
+Effortless Queries, Instant Solutions!
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default About
