18f2a845012a216a9a0caf923525fd48db9be1d6
kishore <kishoreram10106@gmail.com>
2025-02-06 20:10:19 +0530

    add comments section to the admin dashboard

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -3,6 +3,7 @@ import { useLocation } from 'react-router-dom';
 import DashSidebar from '../components/DashSidebar';
 import DashProfile from '../components/DashProfile';
 import DashPosts from '../components/DashPosts';
+import DashComments from '../components/DashComments';
 import DashUsers from '../components/DashUsers';
 
 export default function Dashboard() {
@@ -26,6 +27,7 @@ export default function Dashboard() {
     {/* posts... */}
     {tab === 'posts' && <DashPosts />}
     {tab === 'users' && <DashUsers />}
+    {tab === 'comments' && <DashComments />}
   </div>
   )
 }
