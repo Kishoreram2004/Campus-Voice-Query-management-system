83c7d937d524ccf8a2eef9b17593dc9f82558837
kishore <kishoreram10106@gmail.com>
2025-02-20 19:47:58 +0530

    update

--- client/src/firebase.js
+++ client/src/firebase.js
@@ -5,7 +5,7 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: process.env.VITE_FIREBASE_API_KEY,
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
   authDomain: "let-s-blog-ea314.firebaseapp.com",
   projectId: "let-s-blog-ea314",
   storageBucket: "let-s-blog-ea314.appspot.com",
